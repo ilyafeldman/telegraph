bcafb1a0a5970688ee2a86ef4c1b06b514ecf1f1
ilyafeldman <ilyafeldm@gmail.com>
2021-09-23 13:39:26 +0300

    edited to recieve an input

--- scrapevis/data_main.py
+++ scrapevis/data_main.py
@@ -2,10 +2,10 @@ import asyncio
 from scrape import get_data
 
 async def main():
-    #channel_name = input('Enter the channel name: ')
-    #total_messages = int(input('Enter number of messages: '))
+    channel_name = input('Enter the channel name: ')
+    total_messages = int(input('Enter number of messages: '))
     
-    texts_df = await get_data('sputniknewsint', 5000)
+    texts_df = await get_data(channel_name, total_messages)
     print(texts_df)
 
 asyncio.run(main())
