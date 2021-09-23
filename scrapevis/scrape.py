05f41531741fd3e67172cbb7a83e602dc1cf2d73
ilyafeldman <ilyafeldm@gmail.com>
2021-09-23 13:30:18 +0300

    reworked scraping based on async some functions are still not tested

--- scrapevis/scrape.py
+++ scrapevis/scrape.py
@@ -1,7 +1,11 @@
+from asyncio.tasks import sleep
+from os import wait
 import requests as req , re , pandas as pd , aiohttp , asyncio
 from tqdm.asyncio import trange, tqdm
 from tqdm import tqdm
 from bs4 import BeautifulSoup
+from random import randint
+from time import sleep
 
 exception_list = ['durov', 'username', 'telegram', 'communityrules', 'jobsbot', 'antiscam', 'tandroidapk', 'botfather', 'quizbot']
 
@@ -76,7 +80,7 @@ def loop(df , i , start_channel_name, iter_number , scrolls): # runs n cirles pe
                 print('duplicate')
         if i+1==iter:
             df = df.dropna(inplace = True)
-            df = df.replace(r'^s*$', float('NaN'), regex = True)
+            df = df.replace(r'^s*$', float(' '), regex = True)
             df.to_csv('{} iteration for {}.csv'.format(str(i+1),start_channel_name))
             return df
         i += 1
@@ -98,25 +102,24 @@ def get_text(soup): #used in get_message_info
     try:
         message_text = soup.find(class_= ['tgme_widget_message_text' , 'js-message_text']).text
         message_text = str(message_text).replace('\n', ' ')
+        return message_text
     except:
-        message_text = "NaN"
-    return message_text
-
+        return ''
 def get_date(soup): #used in get_message_info
     try:
         message_date = soup.find('time' , class_ = 'datetime')
         message_date = message_date['datetime']
+        return message_date
     except:
-        message_date = 'NaN'
-    return message_date
+        return ''
 
 def get_views(soup): #used in get_message_info
     try:
         message_views = soup.find(class_= 'tgme_widget_message_views').text
         message_views = convert_str_to_number(message_views)
+        return message_views
     except:
-        message_views = "NaN"
-    return message_views
+        return ''
 
 def link_list(url , last_id , limit):
     limit = last_id - limit
@@ -128,32 +131,53 @@ def link_list(url , last_id , limit):
         links.append(url2)
     return links
 
-async def get_message_info(url , session):
+async def get_message_info(url , session , flag):
     message_data = []
     async with session.get(url , ssl=False) as response:
+        #await sleep(randint(1,2))
         html = await response.text()
     soup = BeautifulSoup(html, 'html.parser')
-    try:
+    if lockcheck(soup) == True:
+        sleeptime = randint(5,25)
+        #print("sleeping for " , sleeptime , " seconds")
+        await asyncio.sleep(sleeptime)
+        flag.set()
+    else:
+        flag.set()
         text = get_text(soup)
-    except:
-        text = 'NaN'
-    date = get_date(soup)
-    views = get_views(soup)
-    message_data.append([url , text , date , views])
+        date = get_date(soup)
+        views = get_views(soup)
+        message_data.append([url , text , date , views])
     return message_data
+
+async def get_message_info_sem(url , session , sem , flag):
+    async with sem:
+        return await get_message_info(url , session , flag)
 
 async def get_data(source , limit):
     url , last_id = last_message_url(connect(source) , source)
     links = link_list(url , last_id , limit)
+    sem = asyncio.Semaphore(3)
+    flag = asyncio.Event()
     async with aiohttp.ClientSession() as session:
         tasks = []
+        messages_data = []
         for link in links:
-            task = asyncio.ensure_future(get_message_info(link , session))
+            task = asyncio.ensure_future(get_message_info_sem(link , session , sem , flag))
             tasks.append(task)
-        messages_data = await asyncio.gather(*tasks)
+        for f in tqdm(asyncio.as_completed(tasks) , total=len(tasks)):
+            await flag.wait()
+            messages_data.append(await f)
     messages_data = [val for sublist in messages_data for val in sublist]
     data_df = pd.DataFrame(messages_data , columns=['link' , 'text' , 'date' , 'views'])
+    nan_value = float("NaN")
+    data_df.replace("", nan_value, inplace=True)
+    data_df.dropna(inplace=True)
     data_df = data_df.drop_duplicates(subset='text')
-    print(data_df)
+    return data_df
 
-asyncio.run(get_data('sputniknewsint' , 200))
+def lockcheck(soup):
+    if soup.find(class_= 'tgme_widget_message_error') != None:
+        return True
+    else:
+        return False
