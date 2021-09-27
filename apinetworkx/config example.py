c63d59b3e7d539a1bbe57cd300b5dd45b5027b23
ilyafeldman <ilyafeldm@gmail.com>
2021-09-27 20:43:36 +0300

    uptated file structure

--- apinetworkx/config example.py
+++
@@ -1,9 +0,0 @@
-#api info from https://my.telegram.org
-api_id = 
-api_hash = ''
-
-# use full phone number including + and country code
-phone = '+'
-
-#use your Telegram username without the '@'
-username = ''
--- apinetworkx/main.py
+++
@@ -1,8 +0,0 @@
-from get_data import get_data
-from get_graph import get_graph
-
-channel_name = input('channel name: ')
-total_messages = int(input('total messages: '))
-iterations_number = int(input("Enter the number of iterations: "))
-
-get_graph(get_data(channel_name , total_messages , iterations_number))
